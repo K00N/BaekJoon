37ea50ea7a220e31d93079e25ab99c3d18c995b8
Baek Un Chang <goeu1100@naver.com>
2022-06-25 14:54:08 +0900

    Commit All

---
+++ src/step6_string/num_11654/Main.java
@@ -0,0 +1,13 @@
+package step6_string.num_11654;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int ASCII = (int)sc.nextLine().charAt(0);
+        System.out.println(ASCII);
+    }
+}
+
+
---
+++ src/step6_string/num_11720/Main.java
@@ -0,0 +1,16 @@
+package step6_string.num_11720;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int repeat = sc.nextInt();
+        int answer = 0;
+        String ts = sc.next();
+        for (int i = 0; i < repeat; i++) {
+            answer += Character.getNumericValue(ts.charAt(i));
+        }
+        System.out.println(answer);
+    }
+}
