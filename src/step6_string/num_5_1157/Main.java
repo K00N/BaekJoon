0769b316c0666cb6f69f6827c03900cbd5790c58
Baek Un Chang <goeu1100@naver.com>
2022-06-27 01:21:23 +0900

    step6_String_num_5_1157

--- src/step6_string/num_5_1157/Main.java
+++ src/step6_string/num_5_1157/Main.java
@@ -1,21 +1,34 @@
 package step6_string.num_5_1157;
 
+import java.io.*;
 import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Main {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String words = sc.nextLine();
-        int[] arr = new int[26];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String words = br.readLine();
+        int[] arr = new int[32];
         Arrays.fill(arr, 0);
         int temp;
         for(int i = 0; i < words.length(); i++) {
-            temp = (int) words.charAt(i)-97;
-            if (arr[temp] == -1)
-                arr[temp] = i;
+            temp = (int) words.charAt(i)%65;
+                arr[temp%32] += 1;
         }
-        for(int j = 0; j < 26; j++)
-            System.out.print(arr[j]+" ");
+        int most=0;
+        int arrIndex=0;
+        for(int j = 0; j < 26; j++){
+            if(most<arr[j]) {
+                most = arr[j];
+                arrIndex = j+65;
+            }
+            else if(most == arr[j]){
+                arrIndex = 63;
+            }
+        }
+        bw.write((char)arrIndex);
+        bw.flush();
+        bw.close();
     }
 }
