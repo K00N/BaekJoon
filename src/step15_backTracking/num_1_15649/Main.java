f3cca5ef0d304d2c57d667b26ff15887dfb179fe
koon <goeu1100@gmail.com>
2022-08-16 17:21:08 +0900

    Commit All

---
+++ src/step15_backTracking/num_1_15649/Main.java
@@ -0,0 +1,18 @@
+package step15_backTracking.num_1_15649;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st =new StringTokenizer(br.readLine(), " ");
+        int numRange = Integer.parseInt(st.nextToken());
+        int numCase = Integer.parseInt(st.nextToken());
+
+
+        bw.flush();
+        bw.close();
+    }
+}
